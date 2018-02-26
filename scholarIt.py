6a08a6f7a0a244d9024629b1ab725a5b7abb3049
Yuri-Nakao <y.nakao0617@gmail.com>
2018-02-26 23:09:21 +0900

    Update scholarIt.py

--- scholarIt.py
+++ scholarIt.py
@@ -3,15 +3,20 @@
 #command line or clipboard.
 
 import webbrowser, sys, pyperclip
+
+print("Please input your search word. If you want to set the source or the author, please input those factors after author: or  source:")
+
 if len(sys.argv) > 1:
-    #Get address from command line.
-    # argvs = sys.argv
-    address = ' '.join(sys.argv[1:])
-# print(argvs)
-# print(address)
+    #Get search words from command line.
+    search_words = ' '.join(sys.argv[1:])
 else:
     # Get address from clipboard.
-    address = pyperclip.paste()
+    search_words = pyperclip.paste()
 
-webbrowser.open('https://scholar.google.co.jp/scholar?hl=ja&as_sdt=0%2C5&q=' + address)
+if sys.argv[-1] == 1:
+    print("True")
+    webbrowser.open('https://scholar.google.co.jp/scholar?hl=ja&as_sdt=0%2C5&q=%22' + search_words + "%22" )
+    webbrowser.open('https://scholar.google.co.jp/scholar?hl=ja&as_sdt=0%2C5&q=' + search_words)
+else:
+    webbrowser.open('https://scholar.google.co.jp/scholar?hl=ja&as_sdt=0%2C5&q=' + search_words)
 
